8cbcc7cb6f932049404b4385b5c8a808a6e68bf8
caoyifeng001 <2018734045@qq.com>
2019-01-15 22:08:05 +0000

    整理了一下代码

--- trajectory_error/src/main.cpp
+++ trajectory_error/src/main.cpp
@@ -32,10 +32,8 @@ class trajectory_error
     public:
         trajectory_error();
         int read_file();
-        void pub_groundtruth(std::string& file);
-        void pub_estimated(std::string& file);
         double conputer_error(std::string& file1,std::string& file);
-        void pub_msg(std::string& line,nav_msgs::Path & path,ros::Publisher pub);
+        void pub_msg(std::string& time,Eigen::Vector3d & t,Eigen::Quaterniond & q,nav_msgs::Path & path,ros::Publisher pub);
     private:
 
         double RMSE,err;
@@ -54,108 +52,7 @@ trajectory_error::trajectory_error()
     path_groundtruth.header.stamp=ros::Time::now();
     path_groundtruth.header.frame_id="odom";
 }
-void trajectory_error::pub_groundtruth(std::string& file)
-{
-    std::ifstream fin(file.c_str());  //why c_str() ?  地址 ？
-    if(fin.is_open() == false)
-    {
-        std::cout<<"Read file "<<file<<" Failed"<<std::endl;
-        return ;
-    }
-    std::string line;
-    path_groundtruth.header.stamp=ros::Time::now();
-    path_groundtruth.header.frame_id="odom";
-    while(std::getline(fin, line))
-    {
-        std::stringstream stream(line);
-        std::string sec;
-        std::getline(stream,sec,'.');
-        std::string nsec;
-        std::getline(stream,nsec,' ');
-        geometry_msgs::PoseStamped this_pose_stamped;
-        this_pose_stamped.header.stamp.sec=stringToNum<int>(sec);
-        this_pose_stamped.header.stamp.nsec=stringToNum<int>(nsec);
-        this_pose_stamped.header.frame_id="odom";
 
-        std::string position[3];
-        for(int i=0;i<3;i++)
-        {
-            std::getline(stream,position[i],' ');
-            //std::cout<<position[i]<<" ";
-        }
-        this_pose_stamped.pose.position.x = stringToNum<double>(position[0]);
-        this_pose_stamped.pose.position.y = stringToNum<double>(position[1]);
-        this_pose_stamped.pose.position.y = stringToNum<double>(position[2]);
-
-        std::string orientation[4];
-        for(int i=0;i<4;i++)
-        {
-            std::getline(stream,orientation[i],' ');
-            //std::cout<<i<<" "<<orientation[i]<<" ";
-        }
-        //std::cout<<std::endl;
-        this_pose_stamped.pose.orientation.x = stringToNum<double>(orientation[0]);
-        this_pose_stamped.pose.orientation.y = stringToNum<double>(orientation[1]);
-        this_pose_stamped.pose.orientation.z = stringToNum<double>(orientation[2]);
-        this_pose_stamped.pose.orientation.w = stringToNum<double>(orientation[3]);
-
-        path_groundtruth.poses.push_back(this_pose_stamped);
-        groundtruth_path_pub.publish(path_groundtruth);
-    }
-
-}
-
-void trajectory_error::pub_estimated(std::string& file)
-{
-    std::ifstream fin(file.c_str());  //why c_str() ?  地址 ？
-    if(fin.is_open() == false)
-    {
-        std::cout<<"Read file "<<file<<" Failed"<<std::endl;
-        return ;
-    }
-    std::string line;
-    path_estimated.header.stamp=ros::Time::now();
-    path_estimated.header.frame_id="odom";
-    while(std::getline(fin, line))
-    {
-        std::stringstream stream(line);
-        std::string sec;
-        std::getline(stream,sec,'.');
-        std::string nsec;
-        std::getline(stream,nsec,' ');
-        geometry_msgs::PoseStamped this_pose_stamped;
-        this_pose_stamped.header.stamp.sec=stringToNum<int>(sec);
-        this_pose_stamped.header.stamp.nsec=stringToNum<int>(nsec);
-        this_pose_stamped.header.frame_id="odom";
-        
-        std::string position[3];
-        for(int i=0;i<3;i++)
-        {
-            std::getline(stream,position[i],' ');
-            //std::cout<<position[i]<<" ";
-        }
-        this_pose_stamped.pose.position.x = stringToNum<double>(position[0]);
-        this_pose_stamped.pose.position.y = stringToNum<double>(position[1]);
-        this_pose_stamped.pose.position.y = stringToNum<double>(position[2]);
-
-        std::string orientation[4];
-        for(int i=0;i<4;i++)
-        {
-            std::getline(stream,orientation[i],' ');
-            //std::cout<<i<<" "<<orientation[i]<<" ";
-        }
-        //std::cout<<std::endl;
-        this_pose_stamped.pose.orientation.x = stringToNum<double>(orientation[0]);
-        this_pose_stamped.pose.orientation.y = stringToNum<double>(orientation[1]);
-        this_pose_stamped.pose.orientation.z = stringToNum<double>(orientation[2]);
-        this_pose_stamped.pose.orientation.w = stringToNum<double>(orientation[3]);
-
-        path_estimated.poses.push_back(this_pose_stamped);
-        
-        estimated_path_pub.publish(path_estimated);
-    }
-    
-}
 double trajectory_error::conputer_error(std::string& file_groundtruth,std::string& file_estimated)
 {
     std::ifstream fin_groundtruth(file_groundtruth.c_str());  //why c_str() ?  地址 ？
@@ -178,7 +75,7 @@ double trajectory_error::conputer_error(std::string& file_groundtruth,std::strin
         Eigen::Vector3d t_g, t_e;
         Eigen::Quaterniond Q_g,Q_e; 
         Eigen::Matrix<double,6,1> kesi;
-        double time_g,time_e;
+        std::string time_g,time_e;
         std::stringstream stream_groundtruth(line_groundtruth), stream_estimated(line_estimated);
         stream_groundtruth>>time_g>>t_g[0]>>t_g[1]>>t_g[2]>>Q_g.x()>>Q_g.y()>>Q_g.z()>>Q_g.w();
         stream_estimated>>time_e>>t_e[0]>>t_e[1]>>t_e[2]>>Q_e.x()>>Q_e.y()>>Q_e.z()>>Q_e.w();
@@ -188,8 +85,8 @@ double trajectory_error::conputer_error(std::string& file_groundtruth,std::strin
         err += kesi.transpose() * kesi;
         num++;
          
-        pub_msg(line_groundtruth,path_groundtruth,groundtruth_path_pub);
-        pub_msg(line_estimated,path_estimated,estimated_path_pub);
+        pub_msg(time_g,t_g,Q_g,path_groundtruth,groundtruth_path_pub);
+        pub_msg(time_e,t_e,Q_e,path_estimated,estimated_path_pub);
         
     }
     if(!std::getline(fin_groundtruth, line_groundtruth) 
@@ -201,39 +98,25 @@ double trajectory_error::conputer_error(std::string& file_groundtruth,std::strin
     
 }
 
-void trajectory_error::pub_msg(std::string& line,nav_msgs::Path & path,ros::Publisher pub)
+void trajectory_error::pub_msg(std::string& time,Eigen::Vector3d & t,Eigen::Quaterniond & q,nav_msgs::Path & path,ros::Publisher pub)
 {
-    std::stringstream stream(line);
-    std::string sec;
-    std::getline(stream,sec,'.');
-    std::string nsec;
-    std::getline(stream,nsec,' ');
+    std::stringstream stream(time);
+    std::string sec , nsec; 
+    std::getline(stream,sec,',');
+    std::getline(stream,nsec,',');
     geometry_msgs::PoseStamped this_pose_stamped;
     this_pose_stamped.header.stamp.sec=stringToNum<int>(sec);
     this_pose_stamped.header.stamp.nsec=stringToNum<int>(nsec);
     this_pose_stamped.header.frame_id="odom";
-    
-    std::string position[3];
-    for(int i=0;i<3;i++)
-    {
-        std::getline(stream,position[i],' ');
-        //std::cout<<position[i]<<" ";
-    }
-    this_pose_stamped.pose.position.x = stringToNum<double>(position[0]);
-    this_pose_stamped.pose.position.y = stringToNum<double>(position[1]);
-    this_pose_stamped.pose.position.y = stringToNum<double>(position[2]);
 
-    std::string orientation[4];
-    for(int i=0;i<4;i++)
-    {
-        std::getline(stream,orientation[i],' ');
-        //std::cout<<i<<" "<<orientation[i]<<" ";
-    }
-    //std::cout<<std::endl;
-    this_pose_stamped.pose.orientation.x = stringToNum<double>(orientation[0]);
-    this_pose_stamped.pose.orientation.y = stringToNum<double>(orientation[1]);
-    this_pose_stamped.pose.orientation.z = stringToNum<double>(orientation[2]);
-    this_pose_stamped.pose.orientation.w = stringToNum<double>(orientation[3]);
+    this_pose_stamped.pose.position.x = t[0];
+    this_pose_stamped.pose.position.y = t[1];
+    this_pose_stamped.pose.position.y = t[2];
+
+    this_pose_stamped.pose.orientation.x = q.x();
+    this_pose_stamped.pose.orientation.y = q.y();
+    this_pose_stamped.pose.orientation.z = q.z();
+    this_pose_stamped.pose.orientation.w = q.w();
 
     path.poses.push_back(this_pose_stamped);
     
